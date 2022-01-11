12fbf6e5a1cdc8f6c2743889ca796b6319808e13
yacineBu <burtinyacine2@sfr.fr>
2022-01-11 16:47:04 +0100

    commit de transfert

--- modules/mod_voiture/cont_voiture.php
+++ modules/mod_voiture/cont_voiture.php
@@ -8,55 +8,35 @@ include 'vue_voiture.php';
 include 'modele_voiture.php';
 
 class ContVoiture extends ContGenerique {
-    public $action;
 
     public function __construct () {
         $this->vue = new VueVoiture();
         $this->modele = new ModeleVoiture();
 
-        $this->vue->menu();
-
-        $this->action = $_GET['action'];
-
-        switch ($this->action) {
-            case "bienvenue":
-                $this->bienvenue();
-                break;
-            case "liste":
-                $this->liste();
-                break;
-            case "details":
-                $this->afficheDescription();
-                break;
-            case "ajout":
-                if (!isset($_POST['nomVoiture'])) {
-                    $this->form_ajout();
-                }
-                else{
-                    $this->ajout();
-                }
-        }
+        // Appeller les méthodes du controlleur dans le constructeur ou dans le module, c'est pareil, pas d'importance.
+        $this->presGeneralVoiture();
+        $this->caracteristiques();
+        $this->photoDetails();
     }
 
-    public function bienvenue(){
-        ?><p>Bienvenue!</p><?php
+    public function lienFeuilleCSS() {
+        return $this->vue->lienFeuilleCSS();
     }
 
-    public function liste(){
-        $tab = $this->modele->getListe();
-        $this->vue->affiche_liste($tab);
+    public function presGeneralVoiture() {
+        if (isset($_POST['message']))
+            $this->vue->bandeauConfirmationPublicCom();
+
+        $this->vue->sectionOverview();
+        $this->vue->sectionDescr();
     }
 
-    public function afficheDescription(){
-        $this->vue->affiche_details($this->modele->getDescription());
+    public function caracteristiques() {
+        $this->vue->sectionCaracteristiques();
     }
 
-    public function form_ajout(){
-        $this->vue->form_ajout();
-    }
-
-    public function ajout(){
-        $this->modele->ajout();
+    public function photoDetails() {
+        $this->vue->sectionPhotoDetails();
     }
 }
 ?>
--- modules/mod_voiture/vue_voiture.php
+++ modules/mod_voiture/vue_voiture.php
@@ -1,54 +1,51 @@
 <?php
-    if (!defined('CONST_INCLUDE'))
-        die('Accès non-autorisé.');
 
-    require_once 'vue_generique.php';
-    class VueVoiture extends VueGenerique{
-        public function __construct () {
-            parent::__construct();
-        }
+if (!defined('CONST_INCLUDE'))
+    die('Accès non-autorisé.');
 
-        public function affiche_liste ($tab){
-            echo "<h3>Liste:</h3>";
-
-            foreach ($tab as $cle => $val){
-                $id = $val['id'];
-                echo "<p><a href=index.php?module=Voiture&action=details&id=$id>";
-                echo $val['nom'];
-                echo"</a></p>";
-            }
-        }
-
-        public function menu(){
-            ?>
-                <h3>Menu Voiture:</h3>
-                <p><a href=index.php?module=Voiture&action=bienvenue>Bienvenue</a></p>
-                <p><a href=index.php?module=Voiture&action=liste>Liste</a></p>
-                <p><a href=index.php?module=Voiture&action=ajout>Ajout</a></p>
-            <?php   
-        }
-
-        public function affiche_details($tab){
-            foreach ($tab as $cle => $val){
-                echo "<p>";
-                echo $val['nom'];
-                echo " : ";
-                echo $val['description'];
-                echo"</p>";
-            }
-        }
-
-        public function form_ajout(){
-            ?>
-            <form action="index.php?module=Voiture&action=ajout" method="post">
-                <p>Nom voiture :</p>
-                <input type="text" name="nomVoiture">
-                <p>Description :</p>
-                <input type="text" name="descriptionVoiture">
-                <p></p>
-                <input type="submit" name="sendButton" value="ajouter">
-            </form>
-            <?php
-        }
+require_once 'vue_generique.php';
+class VueVoiture extends VueGenerique{
+    public function __construct () {
+        parent::__construct();
     }
+
+    public function lienFeuilleCSS()
+    {
+        ?>
+        <link rel="stylesheet" type="text/css" href="modules/mod_voiture/style.css">
+        <?php
+    }
+
+    public function bandeauConfirmationPublicCom() {
+        ?>
+
+        <?php
+    }
+
+    public function sectionOverview() {
+        ?>
+
+        <?php
+    }
+
+    public function sectionDescr() {
+        ?>
+
+        <?php
+    }
+
+    public function sectionCaracteristiques() {
+        ?>
+
+        <?php
+    }
+
+    public function sectionPhotoDetails() {
+        ?>
+
+        <?php
+    }
+
+    
+}
 ?>
